a60520cd9c2a24a9449add698985da35ce125b1c
norskemarcus <marc791j@stud.kea.dk>
2022-04-10 18:02:00 +0200

    made print of both arrays next to each other, but - the update gives 10 instead of 1..

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -10,27 +11,19 @@ public class Main {
     private int[] answers = new int[10];
 
 
-
-
     public void mainMenu() {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Hvilken ting vil du helst have med på en øde ø?");
-        System.out.println("1 kæreste\n2 kaffemaskine\n3 Netflix\n4 sofa\n5 løbesko\n6 guitar\n7 slik\n8 hund\n9 bog\n10 øl");
-        System.out.println("Indtast valg (tal): ");
-
-        int input = scanner.nextInt();
-
-        // Definere antal gange spørreundersøgelsen skal spørge folk før loopet stopper?
-
-        // Lave en metode som returnerer tallet som er valgt?
-        //
-
 
 
         int numberOfInterviews = 0;
 
         while (numberOfInterviews < 20) {
+
+            System.out.println("Hvilken ting vil du helst have med på en øde ø?");
+            System.out.println("1 kæreste\n2 kaffemaskine\n3 Netflix\n4 sofa\n5 løbesko\n6 guitar\n7 slik\n8 hund\n9 bog\n10 øl");
+            System.out.println("Indtast valg (tal): ");
+
+            int input = scanner.nextInt();
 
             switch (input) {
                 case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 -> {
@@ -40,7 +33,16 @@ public class Main {
                 default -> System.out.println("Du tastet ikke et tal mellem 1 og 10. Prøv igen");
             }
 
+             // If arrays are of equal length:
+
+            for (int i = 0; i < item.length; i++) {
+            System.out.println(item[i] + '\t' + answers[i]);
+            }
         }
+
+
+
+
     }
 
 
@@ -50,8 +52,15 @@ public class Main {
         }
     }
 
+    @Override
+    public String toString() {
+        return "Main{" +
+            "item=" + Arrays.toString(item) +
+            ", answers=" + Arrays.toString(answers) +
+            '}';
+    }
 
-        public static void main (String[]args){
+    public static void main (String[]args){
 
         Main main = new Main();
         main.mainMenu();

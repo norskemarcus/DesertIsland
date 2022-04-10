431b916c9f852a87f7755dc41da8c1ac4a5b4e09
norskemarcus <marc791j@stud.kea.dk>
2022-04-10 18:17:36 +0200

    changed output, still bug

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,13 @@
 package com.company;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    private String[] item = {"1 kæreste", "2 kaffemaskine", "3 Netflix", "4 sofa", "5 løbesko",
+    private final String[] item = {"1 kæreste", "2 kaffemaskine", "3 Netflix", "4 sofa", "5 løbesko",
         "6 guitar", "7 slik", "8 hund", "9 bog", "10 øl"};
 
-    private int[] answers = new int[10];
+    private final int[] answers = new int[10];
 
 
     public void mainMenu() {
@@ -19,8 +18,8 @@ public class Main {
 
         while (numberOfInterviews < 20) {
 
-            System.out.println("Hvilken ting vil du helst have med på en øde ø?");
-            System.out.println("1 kæreste\n2 kaffemaskine\n3 Netflix\n4 sofa\n5 løbesko\n6 guitar\n7 slik\n8 hund\n9 bog\n10 øl");
+            System.out.println("\nHvilken ting vil du helst have med på en øde ø?");
+            System.out.println("1 kæreste\n2 kaffemaskine\n3 Netflix\n4 sofa\n5 løbesko\n6 guitar\n7 slik\n8 hund\n9 bog\n10 øl\n");
             System.out.println("Indtast valg (tal): ");
 
             int input = scanner.nextInt();
@@ -28,37 +27,26 @@ public class Main {
             switch (input) {
                 case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 -> {
                     updateList(input);
-                    numberOfInterviews += 1;
+                    numberOfInterviews ++;
                 }
-                default -> System.out.println("Du tastet ikke et tal mellem 1 og 10. Prøv igen");
+                default -> System.out.println("Du tastet ikke et tal mellem 1 og 10. Prøv igen\n");
             }
 
              // If arrays are of equal length:
 
             for (int i = 0; i < item.length; i++) {
-            System.out.println(item[i] + '\t' + answers[i]);
+            System.out.println(item[i] + '\t' + "("+answers[i]+")");
             }
         }
-
-
-
-
     }
 
 
     public void updateList(int input){
         for (int i = 0; i < item.length; i++) {
-            answers[input-1] += 1;
+            answers[input-1] ++;
         }
     }
 
-    @Override
-    public String toString() {
-        return "Main{" +
-            "item=" + Arrays.toString(item) +
-            ", answers=" + Arrays.toString(answers) +
-            '}';
-    }
 
     public static void main (String[]args){
 
